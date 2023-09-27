411c3d92fdd7a15f8ab68c37c63abde9529a7957
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-27 16:25:03 -0300

    fix: change update data param type

--- src/modules/data/contracts/product-repository.ts
+++ src/modules/data/contracts/product-repository.ts
@@ -1,9 +1,10 @@
 import { ProductModel } from "../models/product";
+import { UpdateProductModel } from "../models/update-product";
 
 export interface IProductRepository {
   save(data: ProductModel): Promise<void>;
   findUnique(id: string): Promise<ProductModel>;
   findMany(): Promise<ProductModel[]>;
   delete(id: string): Promise<void>;
-  update(data: ProductModel, id: string): Promise<void>;
+  update(id: string, data: UpdateProductModel): Promise<void>;
 }
