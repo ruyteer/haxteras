727995b533f4f183e68ba6f1804212af76a00fc7
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 09:46:04 -0300

    feat: add new method

--- src/modules/data/contracts/category-repository.ts
+++ src/modules/data/contracts/category-repository.ts
@@ -4,5 +4,6 @@ export interface ICategoryRepository {
   save(category: CategoryModel): Promise<void>;
   findMany(): Promise<CategoryModel[]>;
   findUnique(id: string): Promise<CategoryModel>;
+  findFirst(name: string): Promise<CategoryModel>;
   delete(id: string): Promise<void>;
 }
