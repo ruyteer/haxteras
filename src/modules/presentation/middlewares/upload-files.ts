b7476a9c1921e353eb557bda12dec3ca7204c015
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 15:14:16 -0300

    feat: add middleware update files to firebase and save it on req.files.firebaseUrl

---
+++ src/modules/presentation/middlewares/upload-files.ts
@@ -0,0 +1,19 @@
+import { IFirebaseUpload } from "../contracts/firebase-upload";
+import { badResponse, okResponse } from "../helpers/http-response";
+import { Controller, httpRequest, httpResponse } from "../protocols";
+
+export class UploadFile implements Controller {
+  constructor(private readonly firebaseUpload: IFirebaseUpload) {}
+
+  async handle(req: httpRequest): Promise<httpResponse> {
+    try {
+      const files = req.files;
+      const fileUrl = await this.firebaseUpload.uploadFile(files);
+      req.files.firebaseUrl = fileUrl;
+
+      return okResponse();
+    } catch (error) {
+      return badResponse(error);
+    }
+  }
+}
