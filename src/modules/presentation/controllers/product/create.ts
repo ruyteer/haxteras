89c56120bc4fb03195172e52c71d81ec05aaec3c
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 16:03:43 -0300

    fix: change product model because we receive strings on request

--- src/modules/presentation/controllers/product/create.ts
+++ src/modules/presentation/controllers/product/create.ts
@@ -1,21 +1,21 @@
-import { ProductModel } from "../../../data/models/product";
 import { ProductServices } from "../../../data/services/product/product";
 import { badResponse, okResponse } from "../../helpers/http-response";
 import { Controller, httpRequest, httpResponse } from "../../protocols";
+import { ProductViewModel } from "../../views/product";
 
 export class CreateProductController implements Controller {
   constructor(private productServices: ProductServices) {}
 
   async handle(req: httpRequest): Promise<httpResponse> {
     try {
-      const data: ProductModel = req.body;
+      const data: ProductViewModel = req.body;
       const { id } = req.params;
       const files = req.files.firebaseUrl;
 
       await this.productServices.create(id, {
         name: data.name,
-        price: data.price,
-        stock: data.stock,
+        price: parseFloat(data.price),
+        stock: parseInt(data.stock),
         description: data.description,
         images: files,
       });
