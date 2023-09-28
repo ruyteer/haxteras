c541f5f2c8377add14e127bc60d2863119d8b0f5
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 13:20:58 -0300

    refactor: add a unique file to export all of controller folders

---
+++ src/modules/presentation/controllers/index.ts
@@ -0,0 +1,11 @@
+export * from "./product/create";
+export * from "./product/delete";
+export * from "./product/find-many";
+export * from "./product/find-unique";
+export * from "./product/update";
+
+export * from "./category/create";
+export * from "./category/delete";
+export * from "./category/find-all-products";
+export * from "./category/find-many";
+export * from "./category/find-unique";
