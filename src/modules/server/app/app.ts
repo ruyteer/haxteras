b35d7389865076f630b32a91773f537f79363921
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 13:23:59 -0300

    feat: use product routes

--- src/modules/server/app/app.ts
+++ src/modules/server/app/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { productRoutes } from "./routes/product";
 const app = express();
 
 app.use(express.json());
+app.use("/product", productRoutes);
 
 export { app };
