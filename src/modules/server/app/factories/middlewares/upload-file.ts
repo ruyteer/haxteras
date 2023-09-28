6fc1a8525943699a16583092f0ad215fa0c7f115
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 15:20:58 -0300

    fix: change factory name

--- src/modules/server/app/factories/middlewares/upload-file.ts
+++ src/modules/server/app/factories/middlewares/upload-file.ts
@@ -2,7 +2,7 @@ import { FirebaseUpload } from "../../../../infra/services/firebase/upload";
 import { UploadFile } from "../../../../presentation/middlewares/upload-files";
 import { Controller } from "../../../../presentation/protocols";
 
-export function makeProductController(): Controller {
+export function makeUploadFile(): Controller {
   const service = new FirebaseUpload();
 
   return new UploadFile(service);
