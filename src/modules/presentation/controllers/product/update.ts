7a69ea13c19026f765f5112eb9e23d7cc236622b
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-09-28 13:20:09 -0300

    fix: change update controller name

--- src/modules/presentation/controllers/product/update.ts
+++ src/modules/presentation/controllers/product/update.ts
@@ -3,7 +3,7 @@ import { ProductServices } from "../../../data/services/product/product";
 import { badResponse, okResponse } from "../../helpers/http-response";
 import { Controller, httpRequest, httpResponse } from "../../protocols";
 
-export class FindManyProductController implements Controller {
+export class UpdateProductController implements Controller {
   constructor(private productServices: ProductServices) {}
 
   async handle(req: httpRequest): Promise<httpResponse> {
