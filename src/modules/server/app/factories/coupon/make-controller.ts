261fafe9958fc9cdc55729929136ad7b50419937
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-02 10:48:01 -0300

    fix: update name

--- src/modules/server/app/factories/coupon/make-controller.ts
+++ src/modules/server/app/factories/coupon/make-controller.ts
@@ -2,7 +2,7 @@ import { CouponServices } from "../../../../data/services/coupon/coupon";
 import { CouponRepository } from "../../../../infra/repositories/coupon";
 import { Controller } from "../../../../presentation/protocols";
 
-export function makeCategoryController(controller): Controller {
+export function makeCouponController(controller): Controller {
   const repository = new CouponRepository();
   const services = new CouponServices(repository);
   return new controller(services);
