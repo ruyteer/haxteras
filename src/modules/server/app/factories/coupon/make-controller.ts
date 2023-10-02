b2a83a63a45a98bb157df6e8110308f56fa45740
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-02 10:46:42 -0300

    feat: add factory controller

---
+++ src/modules/server/app/factories/coupon/make-controller.ts
@@ -0,0 +1,9 @@
+import { CouponServices } from "../../../../data/services/coupon/coupon";
+import { CouponRepository } from "../../../../infra/repositories/coupon";
+import { Controller } from "../../../../presentation/protocols";
+
+export function makeCategoryController(controller): Controller {
+  const repository = new CouponRepository();
+  const services = new CouponServices(repository);
+  return new controller(services);
+}
