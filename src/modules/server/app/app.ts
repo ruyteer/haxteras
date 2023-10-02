8b61dddacbd8e986c97da6f7d77b953ff150f2f5
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-02 10:50:42 -0300

    feat: use coupon routes on app

--- src/modules/server/app/app.ts
+++ src/modules/server/app/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { categoryRoutes, productRoutes } from "./routes";
+import { categoryRoutes, couponRoutes, productRoutes } from "./routes";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/product", productRoutes);
 app.use("/category", categoryRoutes);
+app.use("/coupon", couponRoutes);
 
 export { app };
