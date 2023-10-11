24b6a94951fea878f97bf1c1d4cc64a0a87c732f
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-11 16:30:12 -0300

    feat: temporary webhook to make some tests

--- src/modules/server/app/app.ts
+++ src/modules/server/app/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
-import { categoryRoutes, couponRoutes, productRoutes } from "./routes";
+import {
+  categoryRoutes,
+  couponRoutes,
+  orderRoutes,
+  productRoutes,
+} from "./routes";
 const app = express();
 
 app.use(express.json());
@@ -8,5 +13,26 @@ app.use(cors());
 app.use("/product", productRoutes);
 app.use("/category", categoryRoutes);
 app.use("/coupon", couponRoutes);
+app.use("/order", orderRoutes);
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const event = req.body;
+
+    switch (event.type) {
+      case "payment_intent.created":
+        break;
+      case "payment_intent.succeeded":
+        console.log("deu certo");
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    res.json({ received: true });
+  }
+);
 
 export { app };
