1168a6e6818e79efb3fc9f5e07785a67a4e08c97
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-11 15:33:20 -0300

    feat: create user entity with billing details

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,18 @@
+type Address = {
+  country: string;
+  cep: string;
+  address: string;
+  number: number;
+  state: string;
+  city: string;
+  neighborhood: string;
+};
+
+export type User = {
+  name: string;
+  surname: string;
+  email: string;
+  address: Address;
+  phone: string;
+  cpf: string;
+};
