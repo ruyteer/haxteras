33f8ece02a6c831519cd89f6af2a7dd18ef6d4be
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-15 17:11:15 -0300

    feat: create order repository contract

---
+++ src/modules/data/contracts/order-repository.ts
@@ -0,0 +1,8 @@
+import { OrderModel } from "../models/order";
+
+export interface IOrderRepository {
+  create(order: OrderModel, products: string[], userId: string): Promise<void>;
+  findOne(id: string): Promise<OrderModel>;
+  findAll(): Promise<OrderModel[]>;
+  delete(id: string): Promise<void>;
+}
