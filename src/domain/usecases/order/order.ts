c326df17b84d7c899aac9605e3786467042218f9
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-15 17:08:21 -0300

    fix: add new model to returns without errors

---
+++ src/domain/usecases/order/order.ts
@@ -0,0 +1,17 @@
+import { Order } from "../../entities/order";
+
+type OrderModel = {
+  date: string;
+  status: "approved" | "canceled" | "refused" | "incomplete" | string;
+  amount: number;
+  paymentMethod: "card" | "pix" | string;
+  voucher: string;
+  paymentIntent: string;
+};
+
+export interface OrderUseCases {
+  create(order: Order, products: string[], userId: string): Promise<void>;
+  findOne(id: string): Promise<OrderModel>;
+  findAll(): Promise<OrderModel[]>;
+  delete(id: string): Promise<void>;
+}
