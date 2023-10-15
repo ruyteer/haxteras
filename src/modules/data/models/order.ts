ce412184df3ef55bf5da8e0b5023b900166d13e4
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-15 17:11:29 -0300

    feat: add order model to returns

---
+++ src/modules/data/models/order.ts
@@ -0,0 +1,10 @@
+import { Order } from "../../../domain/entities/order";
+
+export type OrderModel = {
+  date: string;
+  status: "approved" | "canceled" | "refused" | "incomplete" | string;
+  amount: number;
+  paymentMethod: "card" | "pix" | string;
+  voucher: string;
+  paymentIntent: string;
+};
