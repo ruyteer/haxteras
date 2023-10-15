fbe6a40459131dda9f5c3a6ceae52ecf2b1fa3ae
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-15 13:19:05 -0300

    feat: create order type

--- src/domain/entities/order.ts
+++ src/domain/entities/order.ts
@@ -2,4 +2,7 @@ export type Order = {
   date: string;
   status: "approved" | "canceled" | "refused" | "incomplete";
   amount: number;
+  paymentMethod: "card" | "pix";
+  voucher: string;
+  paymentIntent: string;
 };
