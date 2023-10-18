8c2350b6bb995d4626edd5644d288dfe530bdc67
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2023-10-18 12:37:37 -0300

    refactor: remove unitialize imports

--- src/modules/server/app/routes/order.ts
+++ src/modules/server/app/routes/order.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { makeIntent } from "../factories/order/make-intent";
 import { CreateIntentController } from "../../../presentation/controllers/order/create-intent";
 import { controller } from "../adapters/controller";
-import { WebhookController } from "../../../presentation/controllers/order/webhook";
 import { makeStripe } from "../factories/order/make-stripe";
 import { CreateOrderController } from "../../../presentation/controllers/order/create";
 import { makeWebhook } from "../factories/order/make-webhook";
