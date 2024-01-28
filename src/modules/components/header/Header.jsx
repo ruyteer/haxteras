6e5b8e3988e7dc6408124859d86a69917c41d2f1
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-01-28 18:32:42 -0300

    fix image

--- src/modules/components/header/Header.jsx
+++ src/modules/components/header/Header.jsx
@@ -103,7 +103,7 @@ function Header() {
                       loading="lazy"
                       width="18"
                       height="18"
-                      src="https://haxtera.com/wp-content/uploads/2023/06/10481-1.png"
+                      src="/whatsapp.png"
                       class="attachment-large size-large wp-image-11139"
                       alt=""
                     />{" "}
@@ -116,7 +116,7 @@ function Header() {
                       loading="lazy"
                       width="18"
                       height="18"
-                      src="https://haxtera.com/wp-content/uploads/2023/06/10480-1.png"
+                      src="/discord.png"
                       class="attachment-large size-large wp-image-11140"
                       alt=""
                     />{" "}
