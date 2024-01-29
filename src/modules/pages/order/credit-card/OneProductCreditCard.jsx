be78cf86a737bcfd718aead4ed719c37952b2274
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-01-29 10:27:56 -0300

    fix quantity

--- src/modules/pages/order/credit-card/OneProductCreditCard.jsx
+++ src/modules/pages/order/credit-card/OneProductCreditCard.jsx
@@ -43,7 +43,7 @@ function OneProductCreditCard() {
           products: [
             {
               id,
-              quantity,
+              quantity: parseInt(quantity),
             },
           ],
           quantity,
