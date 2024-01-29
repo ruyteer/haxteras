dd26501df1bc45332e41525b0e4309c16f07e9f9
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-01-29 09:17:06 -0300

    fix bugs styles

--- src/modules/components/menu-items/MenuItems.jsx
+++ src/modules/components/menu-items/MenuItems.jsx
@@ -191,7 +191,7 @@ function MenuItems() {
             ))}
             <tr>
               <td>Crown</td>
-              <td>R$ 0.28</td>
+              <td>R$ 0.18</td>
               <td>
                 <QuantityInput />
               </td>
