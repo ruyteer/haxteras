cde7390f8eac2c9e27b19039688db20c05022ab6
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-01-31 20:08:32 -0300

    add webhook to test

---
+++ src/modules/presentation/controllers/order/mp-webhook.ts
@@ -0,0 +1,24 @@
+import { ICreateIntent } from "../../contracts/create-intent";
+import { badResponse, okResponse } from "../../helpers/http-response";
+import { Controller, httpRequest, httpResponse } from "../../protocols";
+import { MercadoPagoConfig, Preference } from "mercadopago";
+const client = new MercadoPagoConfig({
+  accessToken:
+    "TEST-2348908258126449-041219-4b97b3cbef8459c5def475afe3616243-305234564",
+});
+
+export class MPWebhook implements Controller {
+  async handle(req: httpRequest): Promise<httpResponse> {
+    try {
+      const data = req.body;
+      const params = req.params;
+
+      console.log(data);
+      console.log(params);
+
+      return okResponse();
+    } catch (error) {
+      return badResponse(error);
+    }
+  }
+}
