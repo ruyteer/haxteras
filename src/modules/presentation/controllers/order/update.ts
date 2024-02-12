8b35c0b481020873e367fb640e1932032c1aeec6
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-02-12 08:35:34 -0300

    add nenbot pix

--- src/modules/presentation/controllers/order/update.ts
+++ src/modules/presentation/controllers/order/update.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../../../config/prisma-client";
 import { OrderServices } from "../../../data/services/order/order";
+import { NodemailerServices } from "../../../infra/services/email/nodemailer-services";
 import { badResponse, okResponse } from "../../helpers/http-response";
 import { Controller, httpRequest, httpResponse } from "../../protocols";
 
@@ -9,11 +10,45 @@ export class UpdateOrdersController implements Controller {
   async handle(req: httpRequest): Promise<httpResponse> {
     try {
       const { id } = req.params;
+      const emailServices = new NodemailerServices();
 
-      await prisma.order.update({
+      const order = await prisma.order.update({
         where: { id },
         data: { status: "succeeded" },
       });
+
+      const products = order.products[0].split(" ");
+
+      if (products[0] === "nenbot") {
+        const day = parseInt(products[1]);
+        const quantity = order.quantity;
+
+        const nenbots = await prisma.nenbot.findMany();
+
+        const filteredNenbots = nenbots.filter((result) => result.days === day);
+
+        let nenbotsFinallyArray = [];
+
+        if (filteredNenbots.length < 1) {
+          console.error("No have nenbots");
+        } else {
+          for (let index = 0; index < quantity; index++) {
+            nenbotsFinallyArray.push(filteredNenbots[0]);
+            await prisma.nenbot.delete({
+              where: { id: filteredNenbots[0].id },
+            });
+            filteredNenbots.shift();
+          }
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { id: order.userId },
+        });
+
+        await emailServices.sendNenbotMail(nenbotsFinallyArray, user, order);
+
+        return okResponse();
+      }
 
       return okResponse();
     } catch (error) {
