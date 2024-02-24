d42addafb02fc64963a94d1349629c2f452b851d
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-02-24 12:20:56 -0300

    test

--- src/modules/server/server.ts
+++ src/modules/server/server.ts
@@ -4,10 +4,6 @@ import { createServer } from "http";
 import { app } from "./app/app";
 const server = createServer(app);
 
-export const io = new Server(server, {
-  cors: {
-    origin: process.env.VITE_URL,
-  },
-});
+export const io = new Server(server);
 
-server.listen(3000);
+server.listen(3000, "0.0.0.0");
