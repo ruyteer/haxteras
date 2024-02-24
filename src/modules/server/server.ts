81dc2c667458ceb50e5f3e48c1e2227748c4e870
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-02-24 12:34:48 -0300

    testvasf

--- src/modules/server/server.ts
+++ src/modules/server/server.ts
@@ -4,6 +4,8 @@ import { createServer } from "http";
 import { app } from "./app/app";
 const server = createServer(app);
 
-export const io = new Server(server);
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
 
 server.listen(3000, "0.0.0.0");
