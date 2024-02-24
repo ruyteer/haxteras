9312a35ec876f41cd646baa5c61d6cf9c30e7974
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-02-24 12:35:38 -0300

    asd

--- src/modules/server/server.ts
+++ src/modules/server/server.ts
@@ -5,7 +5,9 @@ import { app } from "./app/app";
 const server = createServer(app);
 
 export const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: {
+    origin: process.env.VITE_URL,
+  },
 });
 
 server.listen(3000, "0.0.0.0");
