a93f7935c1f015ad150c46f6b0dc35611ee8e369
Ruyter Araujo Rocha <ruyteraraujo992@gmail.com>
2024-02-27 09:09:00 -0300

    update backend fix bug

--- src/modules/presentation/controllers/order/update.ts
+++ src/modules/presentation/controllers/order/update.ts
@@ -12,54 +12,62 @@ export class UpdateOrdersController implements Controller {
       const { id } = req.params;
       const emailServices = new NodemailerServices();
 
-      const order = await prisma.order.update({
-        where: { id },
-        data: { status: "succeeded" },
-      });
+      const findOrder = await prisma.order.findUnique({ where: { id } });
 
-      const products = order.products[0].split(" ");
-
-      if (products[0] === "nenbot") {
-        const day = parseInt(products[1]);
-        const quantity = order.quantity;
-
-        const nenbots = await prisma.nenbot.findMany();
-
-        const filteredNenbots = nenbots.filter((result) => result.days === day);
-
-        let nenbotsFinallyArray = [];
-
-        if (filteredNenbots.length < 1) {
-          console.error("No have nenbots");
-        } else {
-          for (let index = 0; index < quantity; index++) {
-            nenbotsFinallyArray.push(filteredNenbots[0]);
-            await prisma.nenbot.delete({
-              where: { id: filteredNenbots[0].id },
-            });
-            filteredNenbots.shift();
-          }
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { id: order.userId },
-        });
-
-        await emailServices.sendNenbotMail(nenbotsFinallyArray, user, order);
-
+      if (findOrder.status === "succeeded") {
         return okResponse();
-      } else if (products[0] === "dashbot") {
-        console.log("Dashbot aprovado");
       } else {
-        const productOrder = await prisma.product.findUnique({
-          where: { id: order.products[0] },
+        const order = await prisma.order.update({
+          where: { id },
+          data: { status: "succeeded" },
         });
-        const updatedStock = productOrder.stock - order.quantity;
 
-        await prisma.product.update({
-          where: { id: order.products[0] },
-          data: { stock: updatedStock },
-        });
+        const products = order.products[0].split(" ");
+
+        if (products[0] === "nenbot") {
+          const day = parseInt(products[1]);
+          const quantity = order.quantity;
+
+          const nenbots = await prisma.nenbot.findMany();
+
+          const filteredNenbots = nenbots.filter(
+            (result) => result.days === day
+          );
+
+          let nenbotsFinallyArray = [];
+
+          if (filteredNenbots.length < 1) {
+            console.error("No have nenbots");
+          } else {
+            for (let index = 0; index < quantity; index++) {
+              nenbotsFinallyArray.push(filteredNenbots[0]);
+              await prisma.nenbot.delete({
+                where: { id: filteredNenbots[0].id },
+              });
+              filteredNenbots.shift();
+            }
+          }
+
+          const user = await prisma.user.findUnique({
+            where: { id: order.userId },
+          });
+
+          await emailServices.sendNenbotMail(nenbotsFinallyArray, user, order);
+
+          return okResponse();
+        } else if (products[0] === "dashbot") {
+          console.log("Dashbot aprovado");
+        } else {
+          const productOrder = await prisma.product.findUnique({
+            where: { id: order.products[0] },
+          });
+          const updatedStock = productOrder.stock - order.quantity;
+
+          await prisma.product.update({
+            where: { id: order.products[0] },
+            data: { stock: updatedStock },
+          });
+        }
       }
 
       return okResponse();
